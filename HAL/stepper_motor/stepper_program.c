213995b24fd23721593acefe4e7a061e19d68265
Soliman162 <ahmedsliman215@gmail.com>
2023-03-02 20:13:19 +0200

    modify Stepper

--- HAL/stepper_motor/stepper_program.c
+++ HAL/stepper_motor/stepper_program.c
@@ -33,7 +33,7 @@ void stepper_voidRotate_CW(u8 Copy_StepMode, u16 degree )
     {
         for( u8 i=0;i<Copy_StepMode;i--)
         {
-            GPIO_enumSETPORTValue( STEPPER_MOTOR_PORT  , (u16)( Port_Value | (Stepper_movment_Mode[Copy_StepMode-4][i]<<COIL_START_PIN) ) );
+            GPIO_enumSETPORTValue( STEPPER_MOTOR_PORT  , (u16)( Port_Value | (Stepper_movment_Mode[Copy_StepMode==4?0:1][i]<<COIL_START_PIN) ) );
             STK_voidSetBusyWait(MAX_DELAY*1000);
             GPIO_enumSETPORTValue( STEPPER_MOTOR_PORT  , (u16)( Port_Value & ~(0b1111<<COIL_START_PIN) ) );
             degree_u16counter +=DEGREE_PER_STEP;
@@ -50,7 +50,7 @@ void stepper_voidRotate_CCW(u8 Copy_StepMode, u16 degree )
     {
         for( u8 i=Copy_StepMode;i>=0;i-- )
         {
-            GPIO_enumSETPORTValue( STEPPER_MOTOR_PORT  , (u16)( Port_Value | (Stepper_movment_Mode[Copy_StepMode-4][i]<<COIL_START_PIN) ) );
+            GPIO_enumSETPORTValue( STEPPER_MOTOR_PORT  , (u16)( Port_Value | (Stepper_movment_Mode[Copy_StepMode==4?0:1][i]<<COIL_START_PIN) ) );
             STK_voidSetBusyWait(MAX_DELAY*1000);
             GPIO_enumSETPORTValue( STEPPER_MOTOR_PORT  , (u16)( Port_Value & ~(0b1111<<COIL_START_PIN) ) );
             degree_u16counter +=DEGREE_PER_STEP;
